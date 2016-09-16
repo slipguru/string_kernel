fabf0ef3baea92d3b37217012893de8539837aca
Federico Tomasi <fdtomasi@gmail.com>
2016-09-16 17:31:55 +0200

    _kernel in SumStringKernel is now saved in a mono-dimensional array.
    TODO do the same for StringKernel

--- string_kernel/core/src/sum_string_kernel.h
+++ string_kernel/core/src/sum_string_kernel.h
@@ -17,19 +17,19 @@ class SumStringKernel {
                   const size_t max_length, double lambda)
       : _min_kn(min_kn), _max_kn(max_kn), _normalize(normalize),
         _symbol_size(symbol_size), _max_length(max_length) {
-            _size = max_kn - min_kn + 1;
-            _string_kernels = new StringKernel<k_type> *[_size];
-            for (size_t i = 0; i < _size; i++) {
+            _num_subseq_length = max_kn - min_kn + 1;
+            _string_kernels = new StringKernel<k_type> *[_num_subseq_length];
+            for (size_t i = 0; i < _num_subseq_length; i++) {
                 // force normalize false in the StringKernel
                 _string_kernels[i] = new StringKernel<k_type>(c, 0, symbol_size, max_length, min_kn + i, lambda);
             }
         }
 
   ~SumStringKernel() {
-        for (size_t i = 0; i < _string_data->size(); i++)
-            delete[] _kernel[i];
+        // for (size_t i = 0; i < _string_data->size(); i++)
+        //     delete[] _kernel[i];
         delete [] _kernel;
-        for (size_t i = 0; i < _size; i++)
+        for (size_t i = 0; i < _num_subseq_length; i++)
             delete _string_kernels[i];
         delete [] _string_kernels;
 
@@ -44,14 +44,14 @@ class SumStringKernel {
   void compute_kernel();
 
   /** Return pointer to kernel matrix. */
-  k_type **values() const {
+  k_type *values() const {
     assert(_kernel);
     return _kernel;
   }
 
   /** Return the size of the array of StringKernels. */
   size_t size() const {
-    return _size;
+    return _num_subseq_length;
   }
 
  protected:
@@ -61,11 +61,11 @@ class SumStringKernel {
   const int _normalize;
   const int _symbol_size;
   const size_t _max_length;
-  size_t _size;
+  size_t _num_subseq_length;
   // const double _lambda;
   DataSet *_string_data;
-  k_type **_kernel;
   // std::vector<StringKernel<k_type> *> _string_kernels;
+  k_type *_kernel;
   StringKernel<k_type> ** _string_kernels;
   k_type *_norms; // keep norms at each step
 };
@@ -77,7 +77,7 @@ void SumStringKernel<k_type>::set_data(const std::vector<std::string> &strings) 
   _string_data = new DataSet(_max_length, _symbol_size);
   _string_data->load_strings(strings);
 
-  for (size_t i = 0; i < _size; i++) {
+  for (size_t i = 0; i < _num_subseq_length; i++) {
       _string_kernels[i]->set_data(strings);
   }
 }
@@ -86,37 +86,36 @@ void SumStringKernel<k_type>::set_data(const std::vector<std::string> &strings) 
 template<class k_type>
 void SumStringKernel<k_type>::compute_kernel() {
   assert(_string_data);
+  size_t kernel_dim = _string_data->size();
 
-  _kernel = new k_type *[_string_data->size()];
-  for (size_t i = 0; i < _string_data->size(); i++)
-    _kernel[i] = new k_type[_string_data->size()];
+  _kernel = new k_type [kernel_dim*kernel_dim];
 
   // Get values for normalization, it is computed for elements in diagonal
-  _norms = new k_type[_string_data->size()];
-  for(size_t i = 0; i < _string_data->size(); i++) {
+  _norms = new k_type[kernel_dim];
+  for(size_t i = 0; i < kernel_dim; i++) {
       _norms[i] = 0;
   }
-  for(size_t i = 0; i < _size; i++) {
+  for(size_t i = 0; i < _num_subseq_length; i++) {
       _string_kernels[i]->compute_kernel();
       if(_normalize) {
           _string_kernels[i]->compute_norms();
-          for (size_t j = 0; j < _string_data->size(); j++) {
-            _norms[j] += _string_kernels[i]->norms[j];
-        }
+          for (size_t j = 0; j < kernel_dim; j++) {
+              _norms[j] += _string_kernels[i]->norms[j];
+          }
       }
   }
 
   for (size_t i = 0; i < _string_data->size(); i++) {
-      if(_normalize) _kernel[i][i] = 1;
+      if(_normalize) _kernel[i*kernel_dim+i] = 1;
       for (size_t j = _normalize ? i + 1 : i; j < _string_data->size(); j++) {
-          _kernel[i][j] = 0;
-          for(size_t k = 0; k < _size; k++) {
-              _kernel[i][j] += _string_kernels[k]->_kernel[i][j];
+          _kernel[i*kernel_dim+j] = 0;
+          for(size_t k = 0; k < _num_subseq_length; k++) {
+              _kernel[i*kernel_dim+j] += _string_kernels[k]->_kernel[i][j];
           }
           if (_normalize) {
-              _kernel[i][j] /= sqrt(_norms[i] * _norms[j]);
+              _kernel[i*kernel_dim+j] /= sqrt(_norms[i] * _norms[j]);
           }
-          _kernel[j][i] = _kernel[i][j];
+          _kernel[j*kernel_dim+i] = _kernel[i*kernel_dim+j];
       }
   }
 }
